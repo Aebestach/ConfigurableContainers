ccc54165e2df4d25476832dde372a9a9ec0fdcaf
Aebestach <aebestach@163.com>
2026-07-15 17:18:43 +0800

    Migrate build system to KSPBuildTools SDK-style projects.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,41 +1,4 @@
-//   AssemblyInfo.cs
-//
-//  Author:
-//       Allis Tauri <allista@gmail.com>
-//
-//  Copyright (c) 2016 Allis Tauri
-
 using System;
-using System.Reflection;
-
-// Information about this assembly is defined by the following attributes.
-// Change them to the values specific to your project.
-
-[assembly: AssemblyTitle("ConfigurableContainers")]
-[assembly: AssemblyDescription("Plugin for developers of part addons for Kerbal Space Program")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Allis Tauri")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-#if NIGHTBUILD
-[assembly: AssemblyVersion("2.6.*")]
-#else
-[assembly: AssemblyVersion("2.6.2.1")]
-#endif
-[assembly: KSPAssembly("ConfigurableContainers", 2, 6)]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
 
 namespace AT_Utils
 {
@@ -47,7 +10,7 @@ namespace AT_Utils
             MaxKSPVersion = new Version(1, 12, 3);
 
             VersionURL =
-                "https://github.com/allista/ConfigurableContainers/tree/master/GameData/ConfigurableContainers/ConfigurableContainers.version";
+                "https://github.com/Aebestach/ConfigurableContainers/raw/master/GameData/ConfigurableContainers/ConfigurableContainers.version";
             UpgradeURL = "https://spacedock.info/mod/1002/Configurable%20Containers";
         }
     }
